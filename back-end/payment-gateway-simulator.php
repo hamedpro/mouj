81edbaf316067d3f15660910610119d7454084ee
hamedpro <hamedyaghootpour@gmail.com>
2021-05-09 21:49:27 +0430

    delete old files

--- back-end/payment-gateway-simulator.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "
-    <script>window.location.assign('../index.html#/payment-success/22')</script>
-";
--- front-end/components/user-home/user-home.js
+++
@@ -1,7 +0,0 @@
-var template = 
-`
-<div id='user-home' class="content-container"></div>
-`;
-Vue.component("user-home",{
-    template
-})
