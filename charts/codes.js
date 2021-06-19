b7b556876158f64e7c36cb3e8d81eb4c5ab82185
hamedpro <hamedyaghootpour@gmail.com>
2021-06-20 00:32:22 +0430

    finish last done work card and timeline boxes

--- charts/codes.js
+++ charts/codes.js
@@ -9,7 +9,13 @@ var updateLastDoneWork = function(){
     .then(res=> res.json())
     .then(res=>{
         var items = [];
-        items.push('hamed')
+        items.push('شروع: '+res.start_date)
+        if(res.end_date==null){
+            items.push('وضعیت: خاتمه نیافته')
+        }
+        items.push('مبلغ کلی: '+res.final_amount_as_rial);
+        items.push('مبلغ جمع شده فعلی: '+res.current_amount);
+
         items.forEach(item=>{
             tmp = document.createElement('div');
             tmp.innerHTML = item
@@ -20,6 +26,29 @@ var updateLastDoneWork = function(){
         els.progressbar.style.width = ((res.current_amount/res.final_amount_as_rial)*100)+"%";
         
     });
+    fetch('../api/requests.php?func_name=get_plan_ids')
+    .then(res=>res.json())
+    .then(res=>{
+        res.forEach(plan_id=>{
+            fetch('../api/requests.php?func_name=get_plan_transactions&plan_id='+plan_id)
+            .then(r=>r.json())
+            .then(r=>{
+                title = "موج شماره: "+plan_id
+                items = []
+                r.forEach(i=>{
+                    items.push({
+                        title:'پرداخت شده توسط: '+i.username,
+                        content:'مبلغ: '+i.amount+' ریال'+' / '+'دسته مقصد: '+(i.category == "mouj"? 'طرح موج':'گروه آنسه الشهدا')
+                    })
+                })
+                document.getElementById('timelineBoxesContainer').innerHTML = document.getElementById('timelineBoxesContainer').innerHTML+componise.renderComponent({
+                    title,
+                    componentName:'timelineBox',
+                    items
+                })
+            })
+        })
+    })
     
     
 }
