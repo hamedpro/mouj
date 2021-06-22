54d8473b30b630d286f3161f68524b4e24b658e8
hamedpro <hamedyaghootpour@gmail.com>
2021-06-22 12:11:12 +0430

    fix issue about last done work in charts page

--- charts/codes.js
+++ charts/codes.js
@@ -3,29 +3,51 @@ var updateLastDoneWork = function(){
         title:document.getElementById('title'),
         progressLabel:document.getElementById('progressLabel'),
         progressbar:document.getElementById('progressbar'),
-        items:document.getElementById('items')
+        items:document.getElementById('items'),
+        progress:document.getElementById('progress')
     }
-    fetch('../api/requests.php?func_name=get_last_plan_data')
-    .then(res=> res.json())
-    .then(res=>{
-        var items = [];
-        items.push('شروع: '+res.start_date)
-        if(res.end_date==null){
-            items.push('وضعیت: خاتمه نیافته')
-        }
-        items.push('مبلغ کلی: '+res.final_amount_as_rial);
-        items.push('مبلغ جمع شده فعلی: '+res.current_amount);
+    fetch('../api/requests.php?func_name=get_plan_ids')
+    .then(res=>res.json())
+    .then(r=>{
+        if(r.length == 0){
+            
+            els.progressLabel.parentElement.removeChild(els.progressLabel)
+            els.progressbar.parentElement.removeChild(els.progressbar)
+            els.items.parentElement.removeChild(els.items)
+            els.progress.parentElement.removeChild(els.progress)
+            els.title.parentElement.removeChild(els.title)
+            empty = document.createElement('h1')
+            empty.classList.add('empty')
+            empty.classList.add('text-light')
+            empty.classList.add('bg-danger')
 
-        items.forEach(item=>{
-            tmp = document.createElement('div');
-            tmp.innerHTML = item
-            els.items.appendChild(tmp)
-        })
-        els.title.innerHTML = res.title;
-        els.progressLabel.innerHTML = res.current_amount +' از '+res.final_amount_as_rial;
-        els.progressbar.style.width = ((res.current_amount/res.final_amount_as_rial)*100)+"%";
+            empty.innerHTML = "there is not any plan";
+            document.getElementsByClassName('lastDoneWork')[0].appendChild(empty)
+        }else{
+            fetch('../api/requests.php?func_name=get_last_plan_data')
+            .then(res=> res.json())
+            .then(res=>{
+                var items = [];
+                items.push('شروع: '+res.start_date)
+                if(res.end_date==null){
+                    items.push('وضعیت: خاتمه نیافته')
+                }
+                items.push('مبلغ کلی: '+res.final_amount_as_rial);
+                items.push('مبلغ جمع شده فعلی: '+res.current_amount);
         
-    });
+                items.forEach(item=>{
+                    tmp = document.createElement('div');
+                    tmp.innerHTML = item
+                    els.items.appendChild(tmp)
+                })
+                els.title.innerHTML = res.title;
+                els.progressLabel.innerHTML = res.current_amount +' از '+res.final_amount_as_rial;
+                els.progressbar.style.width = ((res.current_amount/res.final_amount_as_rial)*100)+"%";
+                
+            });
+        }
+    })
+    
     fetch('../api/requests.php?func_name=get_plan_ids')
     .then(res=>res.json())
     .then(res=>{
