312155f475e18c46f834a2c4097aa0a01320c38e
hamedpro <hamedyaghootpour@gmail.com>
2021-09-16 20:14:30 +0430

    add one options to admin page in settings section (new plan)

--- src/components/admin/sections/SettingsSection.js
+++ src/components/admin/sections/SettingsSection.js
@@ -1,9 +1,45 @@
 import { Component } from "react";
-
+import {custom_ajax, custom_ajax_default_path} from "../../../api_client/custom_ajax"
+import Option from "../../Option/Option"
+import IntroBox from "../../IntroBox/component"
+import box_arrow_up_white from "../../../common/bootstrap-icons/box-arrow-up-white.svg"
+var options = [
+    {
+        title:'new plan',
+        info:"some info about create new plan option",
+        handler(){
+            var starter_username = window.prompt('enter starter username')
+            var final_amount_as_rial = window.prompt('enter final amount as rial')
+            var title = window.prompt('enter its title')
+            var description = window.prompt('enter its description')
+            custom_ajax({
+                params:{
+                    func:'new_plan',
+                    starter_username,
+                    final_amount_as_rial,
+                    title,
+                    description
+                }
+            })
+            .then(r=>{
+                alert('new plan saved successfuly !')
+            })
+        }
+    }
+]
 export default class SettingsSection extends Component{
     render(){
         return(
-            <div>settings section</div>
+            <div>
+                <IntroBox title="settings" info="you can modify settings here" />
+                {options.map(option=>{
+                    return (
+                        <Option title={option.title} content={option.info}>
+                            <img className="" alt="arrow white svg" src={box_arrow_up_white} onClick={option.handler}/>
+                        </Option>
+                    )
+                })}
+            </div>
         )
     }
 }
