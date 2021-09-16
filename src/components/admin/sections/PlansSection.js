f6b0e32b5edf0788314b0b8e9da87d089cd93bea
hamedpro <hamedyaghootpour@gmail.com>
2021-09-16 20:53:44 +0430

    working on admin page sections => plans section and transactions section , also add favicon

--- src/components/admin/sections/PlansSection.js
+++ src/components/admin/sections/PlansSection.js
@@ -1,9 +1,54 @@
 import { Component } from "react";
 import IntroBox from "../../IntroBox/component"
+import Option from "../../Option/Option"
+import {custom_ajax, custom_ajax_default_path} from "../../../api_client/custom_ajax"
+import trash_white_svg from "../../../common/bootstrap-icons/trash-white.svg"
 export default class SettingPagePlansSection extends Component{
+    constructor(){
+        super()
+        this.state = {
+            plans:[]
+        }
+    }
+    reload_plans = ()=>{
+        custom_ajax({
+            params:{
+                func:"get_plans"
+            }
+        })
+        .then(plans=>{
+            this.setState({
+                plans:plans
+            })
+        })
+    }
+    delete_plan = (plan_id)=>{
+        if (! window.confirm('are you sure you want to delete this plan ? ')) return
+        custom_ajax({
+            params:{
+                func:"delete_plan",
+                plan_id
+            }
+        })
+        .finally(r=>{
+            this.reload_plans()
+        })
+    }
+    componentDidMount(){
+        this.reload_plans()
+    }
     render = ()=>(
         <div>
-            <IntroBox title="hamed" info="negin" />
+            <IntroBox title="plans" info="you can modify plans here" />
+            {
+                this.state.plans.map(plan=>{
+                    return (
+                        <Option title={plan.title} content={plan.description} key={plan.id}>
+                            <img alt='trash white svg' src={trash_white_svg} onClick={()=>this.delete_plan(plan.id)}></img>
+                        </Option>
+                    )
+                })
+            }
         </div>
     )
 }
