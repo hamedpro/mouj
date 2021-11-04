0870c764e5b7d4e5fd64e7d2b5a1f683862f364c
hamedpro <hamedyaghootpour@gmail.com>
2021-11-04 11:05:17 +0330

    fix file permitions problem on unix based machines (in building for production)

--- after_build_script.js
+++ after_build_script.js
@@ -5,3 +5,8 @@ console.log("project was built according to these env vars -> ")
 console.log(env_vars_as_json)
 console.log('')
 console.log('[mouj-project] folder is ready to deploy')
+console.log(`[note] : if you want to deploy on a real server,
+you must change permitions of php files and folders which contain them 
+(for example with chmod command on linux),
+set that folders permition to 755 and set php files permitions to 644,
+otherwise you will get internal server error`)
